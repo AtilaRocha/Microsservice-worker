63935cdd7ff5ce2c3fdb35dad3f18c7f6d2ee6e3
Atila Rocha <72106377+AtilaRocha@users.noreply.github.com>
2022-09-27 15:17:22 -0300

    hr-orker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Átila', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Darah', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Morgana', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Athena', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Nina', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
